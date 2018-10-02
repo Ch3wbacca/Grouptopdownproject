f5912ed49152dd705da53cabd98065ae11539d7b
Preston Ryan <20.preston.ryan@ksd.org>
2018-10-02 11:43:46 -0700

    group project

---
+++ Group Project #1/Assets/Scripts/EnemyHP.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHP : MonoBehaviour {
+    public int health = 10;
+    
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+            health--;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+           
+        }
+    }
+}
---
+++ Group Project #1/Assets/Scripts/Shoot.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+    public GameObject prefab;
+    public float shootSpeed = 10;
+    float timer = 0;
+    public float fireRate = 0.3f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (Input.GetButton("Fire1") && timer > fireRate) 
+        {
+            timer = 0;
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            mousePosition.z = 0;
+            Vector3 shootDir = mousePosition - transform.position;
+            shootDir.Normalize();
+            //destination - start position
+            GameObject bullet = (GameObject)Instantiate(prefab,
+                transform.position, Quaternion.identity);
+            bullet.GetComponent<Rigidbody2D>().velocity = shootDir * shootSpeed;
+            Destroy(bullet, 0.70f);
+
+        }
+	}
+}
